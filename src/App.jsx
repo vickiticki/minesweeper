9635903fd31edd7973af55ba00233bcccea1e4d8
Victoria Tiller <vijotiller@gmail.com>
2021-05-25 13:12:57 -0400

    oof

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
 
 export class App extends Component {
+  // state: {
+  //  board: Array(8).fill(0).map(row => new Array(8).fill(' '))
+  //   id: null,
+  //   state: new,
+  //   mines: 10,
+  // }
+
   render() {
+    const header = 'Minesweeper'
     return <div>Hello, World!</div>
   }
 }
