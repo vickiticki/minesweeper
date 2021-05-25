8896e395f8742b8d0ca88a0047dbb9d28cefa6b2
Victoria Tiller <vijotiller@gmail.com>
2021-05-25 14:11:21 -0400

    idk dude

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,44 @@ export class App extends Component {
   //   state: new,
   //   mines: 10,
   // }
+  state = {
+    board: [
+      Array(8)
+        .fill(0)
+        .map(row => new Array(8).fill(' ')),
+    ],
+    id: null,
+    mines: 10,
+  }
 
+  handleClickCell = (row, column) => {
+    console.log(`You clicked row ${row} column ${column}}`)
+  }
   render() {
-    const header = 'Minesweeper'
-    return <div>Hello, World!</div>
+    return (
+      <div>
+        <h1 className="the top of the page">
+          <header>Minesweeper</header>
+        </h1>
+        <h1>
+          <button>New Game</button>
+        </h1>
+        <ul>
+          {this.state.board.map((boardRow, rowIndex) => {
+            return boardRow.map((cell, columnIndex) => {
+              return (
+                <li
+                  key={columnIndex}
+                  onClick={() => this.handleClickCell(rowIndex, columnIndex)}
+                >
+                  {' '}
+                  {cell}
+                </li>
+              )
+            })
+          })}
+        </ul>
+      </div>
+    )
   }
 }
