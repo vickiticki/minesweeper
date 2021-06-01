1f910fb004dee744d218af2fc8d480a97fab1e57
Victoria Tiller <vijotiller@gmail.com>
2021-05-31 20:13:23 -0400

    changed some colors

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,36 +1,36 @@
 import React, { Component } from 'react'
 
 class Gameboard extends Component {
-  // getClassName = cell => {
-  //   switch (cell) {
-  //     case '_':
-  //       return 'isEmpty'
-  //     case 'F':
-  //       return 'isFlag'
-  //     case '*':
-  //       return 'isBomb'
-  //     case '@':
-  //       return 'isFlaggedBomb'
-  //     case '1':
-  //       return 'is1'
-  //     case '2':
-  //       return 'is2'
-  //     case '3':
-  //       return 'is3'
-  //     case '4':
-  //       return 'is4'
-  //     case '5':
-  //       return 'is5'
-  //     case '6':
-  //       return 'is6'
-  //     case '7':
-  //       return 'is7'
-  //     case '8':
-  //       return 'is8'
-  //     default:
-  //       return 'cell'
-  //   }
-  // }
+  getClassName = cell => {
+    switch (cell) {
+      case '_':
+        return 'isEmpty cell'
+      case 'F':
+        return 'isFlag cell'
+      case '*':
+        return 'isBomb cell'
+      case '@':
+        return 'isFlaggedBomb cell'
+      // case '1':
+      //   return 'is1 cell'
+      // case '2':
+      //   return 'is2 cell'
+      // case '3':
+      //   return 'is3 cell'
+      // case '4':
+      //   return 'is4 cell'
+      // case '5':
+      //   return 'is5 cell'
+      // case '6':
+      //   return 'is6 cell'
+      // case '7':
+      //   return 'is7 cell'
+      // case '8':
+      //   return 'is8 cell'
+      default:
+        return 'other cell'
+    }
+  }
   render() {
     return (
       <div className="whole board">
@@ -40,12 +40,12 @@ class Gameboard extends Component {
               {boardRow.map((cell, columnIndex) => {
                 return (
                   <div
-                    className="indidual cell"
                     key={columnIndex}
                     onClick={() => this.props.leftClick(rowIndex, columnIndex)}
                     onContextMenu={() =>
                       this.props.rightClick(rowIndex, columnIndex)
                     }
+                    className={this.getClassName(cell)}
                   >
                     {cell}
                   </div>
